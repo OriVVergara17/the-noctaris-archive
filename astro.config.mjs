2ac91ed4bf6008e8dfae4a9366fbb30d1c64793d
Oriana Vértiz <orianayvertizvergara@outlook.com>
2026-08-20 18:18:23 -0500

    config: add site and base path for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-import mdx from '@astrojs/mdx';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  site: 'https://OriVVergara17.github.io',
+  base: '/the-noctaris-archive',
 });
