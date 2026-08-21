640ac18c719bcac6ca105222ee202f44c1e5e9a0
Oriana Vértiz <orianayvertizvergara@outlook.com>
2026-08-20 20:18:53 -0500

    Fix lowercase site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://OriVVergara17.github.io',
-  base: '/the-noctaris-archive/',
+  site: 'https://orivvergara17.github.io',
+  base: '/the-noctaris-archive',
 });
