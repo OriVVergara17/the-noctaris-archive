a8e969ad08baf9d8b11fe8d7db4654d471fb33a0
Oriana Vértiz <orianayvertizvergara@outlook.com>
2026-08-20 19:47:44 -0500

    Fix: Configurar base path para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://OriVVergara17.github.io',
-  base: '/the-noctaris-archive',
+  base: '/the-noctaris-archive/',
 });
