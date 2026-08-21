a929be2cff24dc090f2ce1b1464501b568dcc9c0
Oriana Vértiz <orianayvertizvergara@outlook.com>
2026-08-20 22:12:23 -0500

    Fix GitHub Pages 404 routes with directory format

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://orivvergara17.github.io',
-  base: '/the-noctaris-archive',
+  base: '/the-noctaris-archive/',
+  build: {
+    format: 'directory'
+  }
 });
